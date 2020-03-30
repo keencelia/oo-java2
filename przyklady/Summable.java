fc462a80f80360e84294028ac8e119b571a98ee0
Ela <ela@ela-mac-air.local>
2020-03-30 17:08:34 +0200

    update

---
+++ przyklady/Summable.java
@@ -0,0 +1,9 @@
+package mini.java.basic.interfaces;
+
+/**
+ * Klasa implementująca ten interfejs potrafi się zsumować
+ * @param <T> Sumowany typ
+ */
+public interface Summable<T> {
+    T sum();
+}
